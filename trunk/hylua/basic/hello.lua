87e24fe36b56b7f5cad30a50bfe3a1bd2961e75a
fox000002 <fox000002@6e3ff0fc-b28b-11de-ae6a-cd50274c347e>
2009-10-17 10:03:36 +0000

    Add hello.lua.

---
+++ trunk/hylua/basic/hello.lua
@@ -0,0 +1,39 @@
+#!/usr/bin/env lua
+
+-- hello.lua
+print("Hello World!");
+
+print("one line\nnext line\n\"in quotes\", 'in quotes'");
+
+page = [[
+    <HTML>
+    <HEAD>
+    <TITLE>An HTML Page</TITLE>
+    </HEAD>
+    <BODY>
+     <A HREF="http://www.lua.org">Lua</A>
+    </BODY>
+    </HTML>
+    ]];
+    
+print(page);
+
+--
+line = io.read()     -- read a line
+n = tonumber(line)   -- try to convert it to a number
+if n == nil then
+  error(line .. " is not a valid number")
+else
+  print(n*2)
+end
+
+-- table
+a = {}     -- create a table and store its reference in `a'
+k = "x"
+a[k] = 10        -- new entry, with key="x" and value=10
+a[20] = "great"  -- new entry, with key=20 and value="great"
+print(a["x"])    --> 10
+k = 20
+print(a[k])      --> "great"
+a["x"] = a["x"] + 1     -- increments entry "x"
+print(a["x"])    --> 11
