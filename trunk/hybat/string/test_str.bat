4f94d5b31873aff2de80566de1b1dc821bb36ab8
fox000002 <fox000002@6e3ff0fc-b28b-11de-ae6a-cd50274c347e>
2010-06-21 04:31:54 +0000

    Add hybat/string.

---
+++ trunk/hybat/string/test_str.bat
@@ -0,0 +1,30 @@
+@echo off
+
+
+
+REM
+color 02
+
+REM
+set origin="This is the origin text!"
+
+echo origin : %origin%
+
+
+REM
+REM
+REM
+
+REM
+REM
+REM
+
+REM
+REM
+REM
+
+REM
+REM
+REM
+
+@pause
